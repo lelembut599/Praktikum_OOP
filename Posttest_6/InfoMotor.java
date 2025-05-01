83378a49dc4bbd7d73ad289d31b09a0f4f7ffdb5
Chrsitian Mahatma Bima Alpindo <christianmba01@gmail.com>
2025-05-01 17:53:02 +0800

    Initial commit

---
+++ Posttest_6/InfoMotor.java
@@ -0,0 +1,6 @@
+package Posttest_6;
+
+public interface InfoMotor {
+    void tampilkanInfo();
+    String getInfo();
+}
---
+++ Posttest_6/MotorMatic.java
@@ -0,0 +1,12 @@
+package Posttest_6;
+
+public class MotorMatic extends Motor {
+    public MotorMatic(String id, String merk, double hargaSewa) {
+        super(id, merk, "Matic", hargaSewa);
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        System.out.println("[MATIC] " + getInfo());
+    }
+}
